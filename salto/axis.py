668080dea0173bbd6433dcb5d2b5a87ec8281cc6
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2022-05-18 12:06:28 +0200

    plot method

--- salto/axis.py
+++ salto/axis.py
@@ -5,8 +5,7 @@ import numpy.typing as npt
 class axis:
     '''
     
-    Create a new axis using the line passing through the point A (negative pole)
-    and B (positive pole).
+    Create a new axis using the line passing through the negative and positive pole.
     
     Parameters
     ----------
@@ -83,7 +82,7 @@ class axis:
                 f'Vector's lenth is {len(V)}, but it should equal {self.dims}' 
             )
         
-        proj_V = self._project(V)
+        proj_V = self._shift_and_project(V)
         dist = np.linalg.norm(proj_V)
         dist_to_shifted_neg_pole = np.linalg.norm(proj_V - self.shifted_negative_pole)
         dist_to_shifted_pos_pole = np.linalg.norm(proj_V - self.shifted_positive_pole)
@@ -91,14 +90,70 @@ class axis:
         value = sign * dist
         return value
     
-    def plot(*args):
-        return        
+    def plot(*args, title = "Embedding vectors", figsize = (10, 5), 
+             poles = {negative: {label: 'Negative pole', color = 'blue'}, 
+                      positive: {label: 'Positive pole', color = 'red'}}):
+        '''
+        
+        Plot the groups of vectors on the on the axis (in the one-dimensional space)
+        
+        Examples
+        --------
+        axis.plot(
+                {values: cold_values, labels: cold, color: 'tab:blue'},
+                {values: warm_values, labels: warm, color: 'tab:red'},
+                poles = {negative: {label: 'ice', color = 'blue'}, 
+                         positive: {label: 'ice', color = 'red'}}
+            )
+   
+        ''' 
+        # Init plot
+        fig, ax = plt.subplots(figsize=figsize, constrained_layout=True)
+        ax.set(title=title)
+        
+        # Generate colours if not defined
+        for group in args:
+            
+            values = group['values']
+            labels = group['labels']
+            color  = group['color']
+            
+            levels = np.tile([-5, 5, -3, 3, -1, 1],
+                 int(np.ceil(len(values)/6)))[:len(values)]
+            
+            ax.vlines(values, 0, levels, color=color)  
+            vert = np.array(['top', 'bottom'])[(levels > 0).astype(int)]
+
+            for d, l, r, va in zip(values, levels, labels, vert):
+                ax.annotate(r, xy=(d, l), xytext=(1, np.sign(l)*3),
+                            textcoords="offset points", va=va, ha="right")
+        
+        # Show poles
+        neg_pole = poles['negative']
+        pos_pole = poles['positive']
+        
+        neg_value = self(self.negative_pole)
+        pos_value = self(self.positive_pole)
+
+        ax.vlines(neg_value, 0, 4, neg_pole['color'])  
+        ax.annotate(neg_pole['label'], xy=(, 4), xytext=(4, np.sign(4)*3),
+            textcoords="offset points", va=va, ha="right", weight='bold')
+
+        ax.vlines(pos_value, 0, 4, color=pos_pole['color'])  
+        ax.annotate(pos_pole['label'], xy=(ice_value, 4), xytext=(4, np.sign(4)*3),
+            textcoords="offset points", va=va, ha="right", weight='bold')
+        
+        # Show the plot
+        ax.yaxis.set_visible(False)
+        ax.spines[["left", "top", "right"]].set_visible(False)
+        ax.margins(y=0.1)
+        plt.show()
 
     def _shift(self, V: npt.NDArray):
         extended_V = _extend_with_one(V)
         return (self.transform @ extended_V)[0:self.dims]
     
-    def _project(self, V: npt.NDArray):
+    def _shift_and_project(self, V: npt.NDArray):
         shifted_V = self.shift(V)
         return (shifted_V.T @ self.direction) / (self.direction.T @ self.direction) * self.direction
     
